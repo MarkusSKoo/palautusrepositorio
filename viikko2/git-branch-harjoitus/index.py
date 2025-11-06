c87b96e98663a36029fdce15b7532cf95ab0405e
MarkusSKoo <markus.kauhanen@helsinki.fi>
2025-11-06 11:25:49 +0200

    Create multiply method

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -3,6 +3,7 @@
 from logger import logger
 from summa import summa
 from erotus import erotus
+from tulo import tulo
 
 logger("aloitetaan ohjelma") # muutos mainissa
 
@@ -11,6 +12,7 @@ y = int(input("luku 2: "))
 
 print(f"{summa(x, y)}") # muutos bugikorjaus-branchissa
 print(f"{erotus(x, y)}") # muutos bugikorjaus-branchissa
+print(f"{x} * {y} = {tulo(x, y)}") 
 
 logger("lopetetaan ohjelma")
 print("goodbye!") # lisäys bugikorjaus-branchissa
